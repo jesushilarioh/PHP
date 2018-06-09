c885ed248965e2c0bf12d6309327d140c2348326
jesushilarioh <jesushilariohernandez@gmail.com>
2018-06-09 10:35:03 -0500

    added implode.php and array_keys.php

--- Function Reference/Text Processing/Strings/String Functions/implode.php
+++ Function Reference/Text Processing/Strings/String Functions/implode.php
@@ -1,5 +1,7 @@
 <?php    # implode - Join array elements with a string
     // Example #1: implode() example
+    // Example #1: implode() example
+    // Example #1: implode() example
     $array = array(
         'lastname',
         'email',
@@ -12,4 +14,32 @@
     // Empty string when using and empty array:
     /* Ex. a */var_dump(implode('hello', array()));
     /* Ex. b */var_dump(implode(', ', $array));
+
+
+
+    // Example #2: in an SQL statement
+    // Example #2: in an SQL statement
+    // Example #2: in an SQL statement
+    $new_user = array (
+        "firstname" => $_POST['firstname'],
+        "lastname" => $_POST['lastname'],
+        "email" => $_POST['email'],
+        "age" => $_POST['age'],
+        "location" => $_POST['location']
+    );
+
+    /**
+     *  Execute the following SQL code:
+     *  INSERT INTO users (firstname, lastname, email, age, location)
+     *      VALUES (:firstname, :lastname, ":email, :age, :location)
+     */
+
+    $sql = sprintf( // sprintf() allows: INSERT INTO x (y) values (:z)
+        "INSERT INTO %s (%s) VALUES (%s)",
+        "users",
+        implode(", ", array_keys($new_user)),
+        ":". implode(", :", array_keys($new_user))
+    );
+
+    print_r(implode(", ", array_keys($new_user)));
 ?>
--- Function Reference/Variable and Type Related Extensions/Arrays/Array Functions/array_keys.php
+++ Function Reference/Variable and Type Related Extensions/Arrays/Array Functions/array_keys.php
@@ -1,11 +1,44 @@
 <?php
 # array_keys — Return all the keys or a subset of the keys of an array
 
+    // Example #1:
+    // Example #1:
+    // Example #1:
     $array = array(
         0 => 100,
         "color" => "red"
     );
+
     $array_keys = array_keys($array);
 
     print_r($array_keys);
+
+
+
+
+    // Example #2: in an SQL statement
+    // Example #2: in an SQL statement
+    // Example #2: in an SQL statement
+    $new_user = array (
+        "firstname" => $_POST['firstname'],
+        "lastname" => $_POST['lastname'],
+        "email" => $_POST['email'],
+        "age" => $_POST['age'],
+        "location" => $_POST['location']
+    );
+
+    /**
+     *  Execute the following SQL code:
+     *  INSERT INTO users (firstname, lastname, email, age, location)
+     *      VALUES (:firstname, :lastname, ":email, :age, :location)
+     */
+
+    $sql = sprintf( // sprintf() allows: INSERT INTO x (y) values (:z)
+        "INSERT INTO %s (%s) VALUES (%s)",
+        "users",
+        implode(", ", array_keys($new_user)),
+        ":". implode(", :", array_keys($new_user))
+    );
+
+    print_r(implode(", ", array_keys($new_user)));
 ?>
